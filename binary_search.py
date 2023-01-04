36e116394b0c8ae68ddb2aaab90b79babbfaf48c
Joao Mateus <joao.amaral@ifam.edu.br>
2023-01-04 13:11:09 -0300

    binary search markdown

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,4 @@
-# def locate_cards(cards, query):
-#     position = 0
-    
-#     while position < len(cards):
-#         if cards[position] == query:
-#             return position
-#         position += 1    
-#     return -1
-
-# list_cards = []
-# nb_query = 5
-# result = locate_cards(list_cards, nb_query)
-# print(result)
-
+# classic implementation of a binary search
 def binary_search(list, item):
     left = 0
     right = len(list) - 1 
@@ -31,5 +18,25 @@ nb_list = [3, 5, 4, 9, 7, 4, 6]
 sorted_list = sorted(nb_list)
 item = 7
 result = binary_search(sorted_list, item)
+print(result)
+
+"""
+Alice has some cards with numbers written on them. She arranges the cards in decreasing order,
+and lays them out face down in a sequence on a table.She challenges Bob to pick out the card
+containing a given number by turning over as few cards as possible. Write a function to help Bob locate the card.
+"""
+# Linear search aproach 
+def locate_cards(cards, query):
+    position = 0
+    
+    while position < len(cards):
+        if cards[position] == query:
+            return position
+        position += 1    
+    return -1
+
+list_cards = []
+nb_query = 5
+result = locate_cards(list_cards, nb_query)
 print(result)
 
