71fc7ff70781da123a4d0479962ca57c44838e45
Joao Mateus <joao.amaral@ifam.edu.br>
2023-01-04 18:12:53 -0300

    recursion binary search example

--- binary_search.py
+++ binary_search.py
@@ -14,10 +14,32 @@ def binary_search(list, item):
             left = middle + 1
     return -1 
         
+# nb_list = [3, 5, 4, 9, 7, 4, 6]
+# sorted_list = sorted(nb_list)
+# item = 7
+# result = binary_search(sorted_list, item)
+# # print(result)
+
+def recursion_binary_search(array, target, low, high):
+    low = 0
+    high = len(array) - 1
+    
+    while low <= high:
+        middle = (low + high) / 2
+        
+        if(array[middle] == target):
+            return middle
+        elif (array[middle] > target):
+            return recursion_binary_search(array, target, low, middle - 1)
+        else: 
+            return recursion_binary_search(array, target, middle + 1, high)
+    return -1
+        
 nb_list = [3, 5, 4, 9, 7, 4, 6]
 sorted_list = sorted(nb_list)
 item = 7
 result = binary_search(sorted_list, item)
+print(sorted_list)
 print(result)
 
 """
@@ -35,8 +57,8 @@ def locate_cards(cards, query):
         position += 1    
     return -1
 
-list_cards = []
-nb_query = 5
-result = locate_cards(list_cards, nb_query)
-print(result)
+# list_cards = []
+# nb_query = 5
+# result = locate_cards(list_cards, nb_query)
+# print(result)
 
