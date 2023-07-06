1c41b88d639b3870cdc01846781c3763aa95655f
Joao Mateus <joao.amaral@ifam.edu.br>
2023-07-05 22:11:40 -0300

    binary search third problem

--- binary_search/03_problem.py
+++ binary_search/03_problem.py
@@ -1,3 +1,39 @@
+"""
+Problem:
+
+Given a sorted integer array, find the index of a given numbers first or last occurrence.
+If the element is not present in the array, report that as well.
+
+**************** Behavior ****************
+
+Input:
+ 
+nums = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9]
+target = 5
+ 
+Output:
+ 
+The first occurrence of element 5 is located at index 1
+The last occurrence of element 5 is located at index 3
+ 
+ 
+Input:
+ 
+nums = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9]
+target = 4
+ 
+Output:
+ 
+Element not found in the array
+
+*******************************
+
+- Following the classic binary search would finish on the first occurrence
+- We have to adjust it to search in the left side to find the first occurrence
+- And in the right side to find the last occurrence
+
+"""
+
 def problem(arr: list, target: int):
     low = 0
     high = len(arr) - 1
