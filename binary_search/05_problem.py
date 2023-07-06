685894c1ac1db1618cc232028c57e8529e7ca80e
Joao Mateus <joao.amaral@ifam.edu.br>
2023-07-06 09:55:41 -0300

    binary search: problem 5

--- binary_search/05_problem.py
+++ binary_search/05_problem.py
@@ -1,4 +1,23 @@
 """
+Find the smallest missing element from a sorted array
 
+Given a sorted array of non-negative distinct integers, find the smallest missing non-negative element in it.
 
 """
+
+def problem(arr: list):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] == mid:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return low
+
+
+print(problem([0, 1, 2, 3, 4, 5, 6]))
