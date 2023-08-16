cd98e035f4558cb10d2ae1103fb42410c3d9f333
Joao Mateus <joao.amaral@ifam.edu.br>
2023-08-16 18:01:12 -0300

    stacks

--- stacks/stacks.py
+++ stacks/stacks.py
@@ -4,26 +4,30 @@ class Stack:
         self.lenght = 0
         self.stack = []
     
-    def is_full(self):
+    def is_full(self) -> bool:
         return len(self.stack) == self.max_lenght
     
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.stack) == 0
     
-    def push(self, value: int):
+    def push(self, value: int) -> None:
         if self.is_full():
             raise Exception("Stack is full")
         
         self.stack.insert(self.lenght, value) 
         self.lenght += 1
+        return f'Element {self.stack[self.lenght - 1]} added to stack'
         
-    def pop(self):
+    def pop(self) -> None:
         if self.is_empty():
             raise Exception("Stack is empty")
         
         self.stack[self.lenght - 1] = None
         self.lenght -= 1
         self.stack = [item for item in self.stack if item is not None]
+        
+        print(f'Element was removed from stack')
+        print(f'Current stack: {self.stack}')
         
 stack_instance = Stack()
 
@@ -33,4 +37,3 @@ stack_instance.push(1)
 print(stack_instance.stack)
 
 stack_instance.pop()
-print(stack_instance.stack)
